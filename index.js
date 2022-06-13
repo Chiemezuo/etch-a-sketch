2cc44012072d0ffdec8bfd23868fede0d83a7dcb
Chiemezuo <chiemezuoakujobi@gmail.com>
2022-06-13 21:33:54 +0100

    Add an erase button functionality

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const container = document.querySelector('.container')
+const eraseButton = document.querySelector('.eraser')
 
 //create a function for making a square div
 function divMaker(num) {
@@ -8,15 +9,17 @@ function divMaker(num) {
     square.style.setProperty('height', `calc(600px/${Math.sqrt(num)})`)
     square.style.backgroundColor = "bisque"
     square.addEventListener('mouseenter', function(e) {
-      e.target.style.backgroundColor = 'black'
+      e.target.style.backgroundColor = "black"
+    })
+
+    eraseButton.addEventListener('click', function(e) {
+      square.addEventListener('mouseenter', function(e) {
+        e.target.style.backgroundColor = "bisque"
+      })
     })
 
     container.appendChild(square)
   }
 }
 
-divMaker(10000)
-// const etcher = document.querySelector('.test')
-// etcher.addEventListener('mouseenter', function(e) {
-//   e.target.style.backgroundColor = 'black'
-// })
+divMaker(8100)
