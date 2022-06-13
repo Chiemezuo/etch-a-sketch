fbc77d2e7e81f41a05a769ddf7c7c7f7b4451b0d
Chiemezuo <chiemezuoakujobi@gmail.com>
2022-06-13 12:40:22 +0100

    Create one div with hover functionality

---
+++ index.js
@@ -0,0 +1,22 @@
+const container = document.querySelector('.container')
+
+//create a function for making a square div
+function divMaker(num) {
+  for (let i = 1; i <= num; i++){
+    const square = document.createElement('div')
+    square.style.width = "700px"
+    square.style.height = "600px"
+    square.style.backgroundColor = "bisque"
+    square.addEventListener('mouseenter', function(e) {
+      e.target.style.backgroundColor = 'black'
+    })
+
+    container.appendChild(square)
+  }
+}
+
+divMaker(1)
+// const etcher = document.querySelector('.test')
+// etcher.addEventListener('mouseenter', function(e) {
+//   e.target.style.backgroundColor = 'black'
+// })
