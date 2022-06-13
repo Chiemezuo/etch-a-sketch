57604176bf2f3328013fe9274b0868d90f306d21
Chiemezuo <chiemezuoakujobi@gmail.com>
2022-06-13 13:05:34 +0100

    Create many squares with local input

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const container = document.querySelector('.container')
 function divMaker(num) {
   for (let i = 1; i <= num; i++){
     const square = document.createElement('div')
-    square.style.width = "700px"
-    square.style.height = "600px"
+    square.style.setProperty('width', `calc(700px/${Math.sqrt(num)})`)
+    square.style.setProperty('height', `calc(600px/${Math.sqrt(num)})`)
     square.style.backgroundColor = "bisque"
     square.addEventListener('mouseenter', function(e) {
       e.target.style.backgroundColor = 'black'
@@ -15,7 +15,7 @@ function divMaker(num) {
   }
 }
 
-divMaker(1)
+divMaker(10000)
 // const etcher = document.querySelector('.test')
 // etcher.addEventListener('mouseenter', function(e) {
 //   e.target.style.backgroundColor = 'black'
